487488c5d91e04909bf7b490faa9a4bc0e53c9b0
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2024-04-29 03:01:52 +0530

    no Validation Pipes

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,14 @@
-import { RequestMethod, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import * as cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api", {
-    exclude: [{ path: "/login", method: RequestMethod.GET }],
-  });
+  app.setGlobalPrefix("api");
   app.use(cookieParser(process.env.COOKIE_SECRET));
-  app.enableCors();
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
+  app.enableCors({ credentials: true });
+  app.use(helmet());
   await app.listen(process.env.PORT);
 }
 bootstrap();
